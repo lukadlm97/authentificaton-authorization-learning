02937a103d9303f2fcce401173e4fca1ac12b27d
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-21 23:16:52 +0200

    setup project

---
+++ Auth/Auth.EntityFramework/DesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.EntityFramework
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=api_auth_Dev;Trusted_Connection=True;MultipleActiveResultSets=true";
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
