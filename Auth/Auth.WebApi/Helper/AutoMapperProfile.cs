b10074119a959175af1c8e96c4bc1a22b4584e55
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-22 03:07:09 +0200

    setup automapper for user and userdto

---
+++ Auth/Auth.WebApi/Helper/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using Auth.Domain.Models;
+using Auth.WebApi.Dtos;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.WebApi.Helper
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User,UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
